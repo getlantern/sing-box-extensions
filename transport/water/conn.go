b3e3e27f25a182ce8b459e167784bfc022fa7a25
WendelHime <6754291+WendelHime@users.noreply.github.com>
2025-04-01 19:15:36 -0300

    feat: adding packet connect

--- transport/water/conn.go
+++ transport/water/conn.go
@@ -4,6 +4,8 @@ import (
 	"net"
 	"sync"
 
+	"github.com/sagernet/sing/common"
+	"github.com/sagernet/sing/common/buf"
 	M "github.com/sagernet/sing/common/metadata"
 )
 
@@ -42,4 +44,64 @@ func (c *WATERConn) Write(b []byte) (n int, err error) {
 	defer c.mu.Unlock()
 	c.handshake = true
 	return c.Conn.Write(b)
+}
+
+func NewWATERPacketConn(conn net.Conn) *waterPacketConn {
+	return &waterPacketConn{Conn: conn}
+}
+
+type waterPacketConn struct {
+	net.Conn
+}
+
+func (c *waterPacketConn) ReadPacket(buffer *buf.Buffer) (M.Socksaddr, error) {
+	_, err := buffer.ReadOnceFrom(c)
+	if err != nil {
+		return M.Socksaddr{}, err
+	}
+	return M.SocksaddrSerializer.ReadAddrPort(buffer)
+}
+
+func (c *waterPacketConn) WritePacket(buffer *buf.Buffer, destination M.Socksaddr) error {
+	defer buffer.Release()
+	header := buf.With(buffer.ExtendHeader(M.SocksaddrSerializer.AddrPortLen(destination)))
+	err := M.SocksaddrSerializer.WriteAddrPort(header, destination)
+	if err != nil {
+		return err
+	}
+	return common.Error(buffer.WriteTo(c))
+}
+
+func (c *waterPacketConn) ReadFrom(p []byte) (n int, addr net.Addr, err error) {
+	n, err = c.Read(p)
+	if err != nil {
+		return
+	}
+	buffer := buf.With(p[:n])
+	destination, err := M.SocksaddrSerializer.ReadAddrPort(buffer)
+	if err != nil {
+		return
+	}
+	if destination.IsFqdn() {
+		addr = destination
+	} else {
+		addr = destination.UDPAddr()
+	}
+	n = copy(p, buffer.Bytes())
+	return
+}
+
+func (c *waterPacketConn) WriteTo(p []byte, addr net.Addr) (n int, err error) {
+	destination := M.SocksaddrFromNet(addr)
+	buffer := buf.NewSize(M.SocksaddrSerializer.AddrPortLen(destination) + len(p))
+	defer buffer.Release()
+	err = M.SocksaddrSerializer.WriteAddrPort(buffer, destination)
+	if err != nil {
+		return
+	}
+	_, err = buffer.Write(p)
+	if err != nil {
+		return
+	}
+	return len(p), nil
 }
