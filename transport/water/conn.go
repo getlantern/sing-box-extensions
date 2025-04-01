d79806cd6c64c19fe1ca736509ebcf7f3885050f
WendelHime <6754291+WendelHime@users.noreply.github.com>
2025-04-01 17:40:41 -0300

    fix: using mutex when reading/writing handshake updates at transport WATER connection

--- transport/water/conn.go
+++ transport/water/conn.go
@@ -2,6 +2,7 @@ package water
 
 import (
 	"net"
+	"sync"
 
 	M "github.com/sagernet/sing/common/metadata"
 )
@@ -10,6 +11,7 @@ type WATERConn struct {
 	net.Conn
 	destination M.Socksaddr
 	handshake   bool
+	mu          sync.Locker
 }
 
 func NewWATERConnection(conn net.Conn, destination M.Socksaddr) *WATERConn {
@@ -17,17 +19,27 @@ func NewWATERConnection(conn net.Conn, destination M.Socksaddr) *WATERConn {
 		Conn:        conn,
 		destination: destination,
 		handshake:   false,
+		mu:          new(sync.Mutex),
 	}
 }
 
+func (c *WATERConn) handshaked() bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.handshake
+}
+
 func (c *WATERConn) Write(b []byte) (n int, err error) {
-	if c.handshake {
+	if c.handshaked() {
 		return c.Conn.Write(b)
 	}
 	err = M.SocksaddrSerializer.WriteAddrPort(c.Conn, c.destination)
 	if err != nil {
 		return
 	}
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.handshake = true
 	return c.Conn.Write(b)
 }
