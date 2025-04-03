15de95d0cedc848d04eaf8dd9bf433e919eff1f9
WendelHime <6754291+WendelHime@users.noreply.github.com>
2025-04-03 11:34:01 -0300

    fix: WithAttrs now concatenate the previous attributes with the new ones

--- log/slog_handler.go
+++ log/slog_handler.go
@@ -25,14 +25,6 @@ func NewLogHandler(logger log.ContextLogger) *slogHandler {
 }
 
 // Enabled reports whether the handler handles records at the given level.
-// The handler ignores records whose level is lower.
-// It is called early, before any arguments are processed,
-// to save effort if the log event should be discarded.
-// If called from a Logger method, the first argument is the context
-// passed to that method, or context.Background() if nil was passed
-// or the method does not take a context.
-// The context is passed so Enabled can use its values
-// to make a decision.
 func (h *slogHandler) Enabled(_ context.Context, level slog.Level) bool {
 	minLevel := slog.LevelDebug
 	if h.opts.Level != nil {
@@ -42,22 +34,6 @@ func (h *slogHandler) Enabled(_ context.Context, level slog.Level) bool {
 }
 
 // Handle handles the Record.
-// It will only be called when Enabled returns true.
-// The Context argument is as for Enabled.
-// It is present solely to provide Handlers access to the context's values.
-// Canceling the context should not affect record processing.
-// (Among other things, log messages may be necessary to debug a
-// cancellation-related problem.)
-//
-// Handle methods that produce output should observe the following rules:
-//   - If r.Time is the zero time, ignore the time.
-//   - If r.PC is zero, ignore it.
-//   - Attr's values should be resolved.
-//   - If an Attr's key and value are both the zero value, ignore the Attr.
-//     This can be tested with attr.Equal(Attr{}).
-//   - If a group's key is empty, inline the group's Attrs.
-//   - If a group has no Attrs (even if it has a non-empty key),
-//     ignore it.
 func (h *slogHandler) Handle(ctx context.Context, record slog.Record) error {
 	if !h.Enabled(ctx, record.Level) {
 		return nil
@@ -94,9 +70,8 @@ func (h *slogHandler) Handle(ctx context.Context, record slog.Record) error {
 
 // WithAttrs returns a new Handler whose attributes consist of
 // both the receiver's attributes and the arguments.
-// The Handler owns the slice: it may retain, modify or discard it.
 func (h *slogHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	h.attrs = parseAttrs(attrs)
+	h.attrs = fmt.Sprintf("%s %s", h.attrs, parseAttrs(attrs))
 	return h
 }
 
@@ -134,23 +109,6 @@ func parseAttrs(attrs []slog.Attr) string {
 
 // WithGroup returns a new Handler with the given group appended to
 // the receiver's existing groups.
-// The keys of all subsequent attributes, whether added by With or in a
-// Record, should be qualified by the sequence of group names.
-//
-// How this qualification happens is up to the Handler, so long as
-// this Handler's attribute keys differ from those of another Handler
-// with a different sequence of group names.
-//
-// A Handler should treat WithGroup as starting a Group of Attrs that ends
-// at the end of the log event. That is,
-//
-//	logger.WithGroup("s").LogAttrs(ctx, level, msg, slog.Int("a", 1), slog.Int("b", 2))
-//
-// should behave like
-//
-//	logger.LogAttrs(ctx, level, msg, slog.Group("s", slog.Int("a", 1), slog.Int("b", 2)))
-//
-// If the name is empty, WithGroup returns the receiver.
 func (h *slogHandler) WithGroup(name string) slog.Handler {
 	// TODO: Implement WithGroup
 	return h
