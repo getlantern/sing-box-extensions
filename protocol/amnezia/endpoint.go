e8b638986046fea2bcfb96abfca30cee02d951c6
garmr <pdixon117@gmail.com>
2025-06-02 15:24:19 -0700

    fix(amnezia): use correct constant for adapter type

--- protocol/amnezia/endpoint.go
+++ protocol/amnezia/endpoint.go
@@ -48,7 +48,7 @@ type Endpoint struct {
 
 func NewEndpoint(ctx context.Context, router adapter.Router, logger log.ContextLogger, tag string, options option.AmneziaEndpointOptions) (adapter.Endpoint, error) {
 	ep := &Endpoint{
-		Adapter:        endpoint.NewAdapterWithDialerOptions(C.TypeWireGuard, tag, []string{N.NetworkTCP, N.NetworkUDP}, options.DialerOptions),
+		Adapter:        endpoint.NewAdapterWithDialerOptions(constant.TypeAmnezia, tag, []string{N.NetworkTCP, N.NetworkUDP}, options.DialerOptions),
 		ctx:            ctx,
 		router:         router,
 		logger:         logger,
