23f3c1b5b5e6276ecbdf8c7e2750a751e8da2d42
garmr-ulfr <104022054+garmr-ulfr@users.noreply.github.com>
2025-06-04 10:57:06 -0700

    remove omitempty json flag
    Co-authored-by: Wendel Hime <6754291+WendelHime@users.noreply.github.com>

--- option/group.go
+++ option/group.go
@@ -2,6 +2,6 @@ package option
 
 type FallbackOutboundOptions struct {
 	// Primary and Fallback are the tags of the primary and fallback outbounds.
-	Primary  string `json:"primary,omitempty"`
-	Fallback string `json:"fallback,omitempty"`
+	Primary  string `json:"primary"`
+	Fallback string `json:"fallback"`
 }
